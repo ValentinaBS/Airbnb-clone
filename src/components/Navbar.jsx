76c93c8df2c3f3ecd8898fb7081984ae0008d47c
unknown <vbsanchez16@gmail.com>
2022-08-16 14:55:22 -0300

    Final Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img 
+                src="/images/airbnb-logo.png" className="nav--logo" 
+                alt="Airbnb logo"
+            />
+        </nav>
+    )
+}
